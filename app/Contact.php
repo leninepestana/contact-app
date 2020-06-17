c673b6a9afa930e712345155d097843c69cdb656
leninepestana <leninepestanana@gmail.com>
2020-06-17 23:07:33 +0100

    13. Eloquent Relationship - Part 1

--- app/Contact.php
+++ app/Contact.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    protected $fillable = ['first_name', 'last_name', 'phone', 'email', 'address'];
+     
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
