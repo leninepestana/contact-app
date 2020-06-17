1709877166e6563ad0a2c0e8ea5d824ef82698c2
leninepestana <leninepestanana@gmail.com>
2020-06-18 00:27:17 +0100

    17. Routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function ()
+{
+    return "<h1>All Contacts";
+})->name('contacts.index');
+
+Route::get('/contacts/create', function ()
+{
+    return "<h1>Add new Contacts";
+})->name('contacts.create');
+
+Route::get('/contacts/{id}', function ($id)
+{
+    return App\Contact::find($id);
+})->name('contacts.show');
