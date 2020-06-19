0b396ee83a8a6c6d9445554b2c10f9889d40e00b
leninepestana <leninepestanana@gmail.com>
2020-06-19 06:22:47 +0100

    27. Exercise 4 - Update Data

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,6 @@ Route::post('/contacts', 'ContactController@store')->name('contacts.store');
 
 Route::get('/contacts/{id}', 'ContactController@show')->name('contacts.show');
 
+Route::put('/contacts/{id}', 'ContactController@update')->name('contacts.update');
+
+Route::get('/contacts/{id}/edit', 'ContactController@edit')->name('contacts.edit');
