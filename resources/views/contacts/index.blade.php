85a2c9ab14bc78753a9c063696d893732ae1a7a3
leninepestana <leninepestanana@gmail.com>
2020-06-19 05:32:52 +0100

    26. Validating and Persisting Data

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -28,6 +28,9 @@
                   </tr>
                 </thead>
                 <tbody>
+                  @if ($message = session('message'))
+                      <div class="alert alert-success">{{ $message }}</div>
+                  @endif
                   @if ($contacts->count())
                       @foreach ($contacts as $index => $contact)
                       <tr>
