9f193e3da7faaaa8416734b40ff21bc873ffb8c4
leninepestana <leninepestanana@gmail.com>
2020-06-21 20:42:46 +0100

    32. Make our Global Scopes reusable

--- app/Scopes/SearchScope.php
+++ app/Scopes/SearchScope.php
@@ -6,17 +6,38 @@ use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Database\Eloquent\Builder;
 
 
-class SearchScope implements Scope {
+class SearchScope implements Scope 
+{
+    // Columns to search in a seacrh criteria
+    protected $searchColumns = [];
+
     public function apply(Builder $builder, Model $model)
     {
-        if ($search = request('search')) {
-            $builder->where('first_name', 'LIKE', "%{$search}%");
-            $builder->orWhere('last_name', 'LIKE', "%{$search}%");
-            $builder->orWhere('email', 'LIKE', "%{$search}%");
-            $builder->orWhereHas('company', function ($query) use ($search)
+        if ($search = request('search')) 
+        {    
+            $columns = property_exists($model, 'searchColumns') ? $model->searchColumns : $this->searchColumns;
+
+            foreach ($columns as $index => $column) 
             {
-                $query->where('name', 'LIKE', "%{$search}%");
-            });
+                $arr = explode('.',$column);
+                $method = $index == 0 ? "where" : "orWhere";
+
+                if (count($arr) == 2) 
+                {
+                    $method .= "Has";
+
+                    list($relationship, $col) = $arr;
+
+                    $builder->$method($relationship, function ($query) use ($search, $col) {
+                        $query->where($col, 'LIKE', "%{$search}%");
+                    });
+                } else {
+                    $builder->$method($column, 'LIKE', "%{$search}%");
+                }
+                
+            }
+            
+             
         }        
     }
 
