e2b5e62439880a8554667baed7bdc631de341b2b
leninepestana <leninepestanana@gmail.com>
2020-06-24 10:59:05 +0100

    43. Displaying Authenticated User's Data

--- app/Contact.php
+++ app/Contact.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    protected $fillable = ['first_name', 'last_name', 'phone', 'email', 'address', 'company_id'];
+    protected $fillable = ['first_name', 'last_name', 'phone', 'email', 'address', 'company_id', 'user_id'];
 
     public $filterColumns = ['company_id'];
      
