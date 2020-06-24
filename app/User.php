c3d1198477be4f4d2c2078e67c064f89fa1d20ad
leninepestana <leninepestanana@gmail.com>
2020-06-24 10:31:42 +0100

    41. Solving Email verification issues, 42. Relationship between User, Company and Contact

--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
@@ -44,6 +44,6 @@ class User extends Authenticatable
    
     public function companies()
     {
-        return $this->hasMany(Contact::class);
+        return $this->hasMany(Company::class);
     }
 }
